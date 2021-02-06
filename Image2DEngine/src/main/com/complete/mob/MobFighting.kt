a3fcaab0dcc7bcbd042c5a47b090ed466c9ad62a
zerowatherblu3 <b2905607825@163.com>
2021-02-06 17:23:28 +0800

    一点小更新...

--- Image2DEngine/src/main/com/complete/mob/MobFighting.kt
+++ Image2DEngine/src/main/com/complete/mob/MobFighting.kt
@@ -5,19 +5,19 @@
 package complete.mob
 
 class MobFighting {
-    var FriendlyEffect : Boolean = true
+    var friendlyEffect : Boolean = true
         get() = field
         set
     var computingHatredLevel : Int? = -1
         get() = field
         set(value) {if (value in -1..10) value else -1}
 
-    fun decideEnemyOrFriendly(FriendlyEffect : Boolean ,Friendly : Int = 0, Enemy : Int = 1): Int {
-        if (FriendlyEffect == true) {
-            return Friendly
+    fun decideEnemyOrFriendly(friendlyEffect : Boolean ,friendly : Int = 0, enemy : Int = 1): Int {
+        if (friendlyEffect == true) {
+            return friendly
         }
         else {
-            return Enemy
+            return enemy
         }
     }
 }
