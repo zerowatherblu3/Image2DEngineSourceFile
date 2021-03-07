5e9711c690a555d6c8acd627a6390ee8012e7385
zerowatherblu3 <b2905607825@163.com>
2021-03-07 13:50:48 +0800

    一点小更新...

---
+++ Image2DEngine/src/main/com/complete/mob/MobDefault.java
@@ -0,0 +1,2 @@
+package complete.mob;public class MobDefault {
+}
---
+++ Image2DEngine/src/main/com/complete/mob/MobTemplate.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2021.  mirror studio
+// This file is created by mirror studio
+// This file is under LGPL license
+
+package complete.mob;
+
+public interface mobTemplate {
+    String mobName = null;
+    double mobHealth = 100;
+
+}
